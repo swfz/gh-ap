dd8b7b509f61cecf12b78e03c9dd2039c81e4e5b
swfz <sawafuji.09@gmail.com>
2022-10-28 18:23:34 +0900

    お試しの更新

--- sandbox/survey.go
+++ sandbox/survey.go
@@ -1,32 +1,58 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/AlecAivazis/survey/v2"
+	"strconv"
 )
 
 func main() {
-	options := []string{"Golang", "Ruby"}
-
-	var tag string
-	q := &survey.Select{
-		Message: "Choose a tag",
-		Options: options,
-		Description: func(value string, index int) string {
-			if value == "Ruby" {
-				return "My favorite Language"
-			}
-			return ""
+	var qs = []*survey.Question{
+		{
+			Name:      "name",
+			Prompt:    &survey.Input{Message: "What is your name?"},
+			Validate:  survey.Required,
+			Transform: survey.Title,
 		},
-		Default: options[0],
+		{
+			Name: "color",
+			Prompt: &survey.Select{
+				Message: "Choose a color:",
+				Options: []string{"red", "blue", "green"},
+				Default: "red",
+			},
+		},
+		{
+			Name:   "age",
+			Prompt: &survey.Input{Message: "How old are you?"},
+			Validate: func(v interface{}) error {
+				//value := reflect.ValueOf(v)
+				//return nil
+				strValue := v.(string)
+				_, err := strconv.ParseFloat(strValue, 64)
+				if err != nil {
+					return errors.New("Value is Int or Float")
+				}
+				return nil
+			},
+		},
 	}
 
-	//err := prompt.SurveyAskOne(q, &tag)
-	//if err != nil {
-	//	fmt.Errorf("could not prompt: %w", err)
-	//}
+	answers := struct {
+		Name          string  // survey will match the question and field names
+		FavoriteColor string  `survey:"color"` // or you can tag fields to match a specific name
+		Age           float64 // if the types don't match, survey will convert it
+	}{}
 
-	survey.AskOne(q, &tag)
+	// perform the questions
+	err := survey.Ask(qs, &answers)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-	fmt.Println(tag)
+	fmt.Printf("%s chose %s.", answers.Name, answers.FavoriteColor)
+
+	fmt.Printf("%+v\n", answers)
 }

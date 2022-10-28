7a1420dc06681a0f41deddfc0145bb85d2585d17
swfz <sawafuji.09@gmail.com>
2022-10-28 19:54:15 +0900

    feat: PR, Issueの番号指定での紐づけ、各種カラムの更新

--- main.go
+++ main.go
@@ -146,21 +146,7 @@ func updateTextProjectField(gqlclient api.GQLClient, projectId string, itemId st
 	fmt.Println(mutation)
 }
 
-func currentPullRequestToProject(gqlclient api.GQLClient, projectId string) (itemId string) {
-	args := []string{"pr", "view", "--json", "id,number,title"}
-	stdOut, _, err := gh.Exec(args...)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(stdOut.String())
-	var currentPR Content
-	if err := json.Unmarshal(stdOut.Bytes(), &currentPR); err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("%+v\n", currentPR)
-
+func addProject(gqlclient api.GQLClient, projectId string, contentId string) (itemId string) {
 	var addProjectMutation struct {
 		AddProjectV2ItemById struct {
 			Item struct {
@@ -170,14 +156,14 @@ func currentPullRequestToProject(gqlclient api.GQLClient, projectId string) (ite
 	}
 
 	addProjectVariables := map[string]interface{}{
-		"contentId": graphql.ID(currentPR.Id),
+		"contentId": graphql.ID(contentId),
 		"projectId": graphql.ID(projectId),
 	}
-	err = gqlclient.Mutate("Assign", &addProjectMutation, addProjectVariables)
+	err := gqlclient.Mutate("Assign", &addProjectMutation, addProjectVariables)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%+v\n", addProjectMutation)
+
 	return addProjectMutation.AddProjectV2ItemById.Item.Id
 }
 
@@ -419,100 +405,21 @@ func main() {
 	}
 	survey.AskOne(typeQuestion, &selectedType)
 
+	var itemId string
 	if selectedType == "Current PullRequest" {
-		itemId := currentPullRequestToProject(gqlclient, selectedProjectId)
-		fmt.Println(itemId)
-
-		for _, field := range fields {
-			if field.DataType == "TEXT" {
-				input := ""
-				prompt := &survey.Input{
-					Message: field.Name,
-				}
-				survey.AskOne(prompt, &input)
-				if input != "" {
-					updateTextProjectField(gqlclient, selectedProjectId, itemId, field.Id, input)
-				}
-			}
-			if field.DataType == "DATE" {
-				qs := []*survey.Question{
-					{
-						Name:   field.Name,
-						Prompt: &survey.Input{Message: field.Name},
-						Validate: func(v interface{}) error {
-							strValue := v.(string)
-							// Allow Zero Value
-							if strValue == "" {
-								return nil
-							}
-							_, err := time.Parse("2006-01-02", strValue)
-							if err != nil {
-								return errors.New("Please format it like this '2006-01-02'")
-							}
-							return nil
-						},
-					},
-				}
-				answers := map[string]interface{}{}
-				err := survey.Ask(qs, &answers)
-				if err != nil {
-					fmt.Println(err.Error())
-					return
-				}
-
-				if answers[field.Name] != "" {
-					updateDateProjectField(gqlclient, selectedProjectId, itemId, field.Id, answers[field.Name].(string))
-				}
-			}
-			if field.DataType == "NUMBER" {
-				qs := []*survey.Question{
-					{
-						Name:   field.Name,
-						Prompt: &survey.Input{Message: field.Name},
-						Validate: func(v interface{}) error {
-							strValue := v.(string)
-							_, err := strconv.ParseFloat(strValue, 64)
-							if err != nil {
-								return errors.New("Value is Int or Float")
-							}
-							return nil
-						},
-					},
-				}
-				answers := map[string]interface{}{}
-				err := survey.Ask(qs, &answers)
-				if err != nil {
-					fmt.Println(err.Error())
-					return
-				}
-				f, _ := strconv.ParseFloat(answers[field.Name].(string), 64)
-				updateNumberProjectField(gqlclient, selectedProjectId, itemId, field.Id, f)
-			}
-			if field.DataType == "SINGLE_SELECT" || field.DataType == "ITERATION" {
-				var selectedOptionId string
-				message := "Choose a " + field.Name
-				fieldOptionSize := len(field.Options)
-				optionIds := make([]string, fieldOptionSize)
-				optionNames := make([]string, fieldOptionSize)
-				for i, opt := range field.Options {
-					optionIds[i] = opt.Id
-					optionNames[i] = opt.Name
-				}
-				q := &survey.Select{
-					Message: message,
-					Options: optionIds,
-					Description: func(value string, index int) string {
-						return optionNames[index]
-					},
-				}
-				survey.AskOne(q, &selectedOptionId)
-				if field.DataType == "ITERATION" {
-					updateIterationProjectField(gqlclient, selectedProjectId, itemId, field.Id, selectedOptionId)
-				} else {
-					updateSingleSelectProjectField(gqlclient, selectedProjectId, itemId, field.Id, selectedOptionId)
-				}
-			}
+		args := []string{"pr", "view", "--json", "id,number,title"}
+		stdOut, _, err := gh.Exec(args...)
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
+		fmt.Println(stdOut.String())
+		var currentPR Content
+		if err := json.Unmarshal(stdOut.Bytes(), &currentPR); err != nil {
+			panic(err)
+		}
+
+		itemId = addProject(gqlclient, selectedProjectId, currentPR.Id)
 	} else {
 		name := selectedType + " Number"
 		qs := []*survey.Question{
@@ -554,6 +461,96 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Printf("%+v\n", content)
+		itemId = addProject(gqlclient, selectedProjectId, content.Id)
+	}
+	for _, field := range fields {
+		if field.DataType == "TEXT" {
+			input := ""
+			prompt := &survey.Input{
+				Message: field.Name,
+			}
+			survey.AskOne(prompt, &input)
+			if input != "" {
+				updateTextProjectField(gqlclient, selectedProjectId, itemId, field.Id, input)
+			}
+		}
+		if field.DataType == "DATE" {
+			qs := []*survey.Question{
+				{
+					Name:   field.Name,
+					Prompt: &survey.Input{Message: field.Name},
+					Validate: func(v interface{}) error {
+						strValue := v.(string)
+						// Allow Zero Value
+						if strValue == "" {
+							return nil
+						}
+						_, err := time.Parse("2006-01-02", strValue)
+						if err != nil {
+							return errors.New("Please format it like this '2006-01-02'")
+						}
+						return nil
+					},
+				},
+			}
+			answers := map[string]interface{}{}
+			err := survey.Ask(qs, &answers)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			if answers[field.Name] != "" {
+				updateDateProjectField(gqlclient, selectedProjectId, itemId, field.Id, answers[field.Name].(string))
+			}
+		}
+		if field.DataType == "NUMBER" {
+			qs := []*survey.Question{
+				{
+					Name:   field.Name,
+					Prompt: &survey.Input{Message: field.Name},
+					Validate: func(v interface{}) error {
+						strValue := v.(string)
+						_, err := strconv.ParseFloat(strValue, 64)
+						if err != nil {
+							return errors.New("Value is Int or Float")
+						}
+						return nil
+					},
+				},
+			}
+			answers := map[string]interface{}{}
+			err := survey.Ask(qs, &answers)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			f, _ := strconv.ParseFloat(answers[field.Name].(string), 64)
+			updateNumberProjectField(gqlclient, selectedProjectId, itemId, field.Id, f)
+		}
+		if field.DataType == "SINGLE_SELECT" || field.DataType == "ITERATION" {
+			var selectedOptionId string
+			message := "Choose a " + field.Name
+			fieldOptionSize := len(field.Options)
+			optionIds := make([]string, fieldOptionSize)
+			optionNames := make([]string, fieldOptionSize)
+			for i, opt := range field.Options {
+				optionIds[i] = opt.Id
+				optionNames[i] = opt.Name
+			}
+			q := &survey.Select{
+				Message: message,
+				Options: optionIds,
+				Description: func(value string, index int) string {
+					return optionNames[index]
+				},
+			}
+			survey.AskOne(q, &selectedOptionId)
+			if field.DataType == "ITERATION" {
+				updateIterationProjectField(gqlclient, selectedProjectId, itemId, field.Id, selectedOptionId)
+			} else {
+				updateSingleSelectProjectField(gqlclient, selectedProjectId, itemId, field.Id, selectedOptionId)
+			}
+		}
 	}
 }

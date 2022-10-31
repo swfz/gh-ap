aa5ccf6b9fadef8dc6cab11dcd3432a0f32dc6a5
swfz <sawafuji.09@gmail.com>
2022-10-31 12:54:32 +0900

    refactor: surveyの処理を関数+ファイル分割した

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"github.com/AlecAivazis/survey/v2"
 	"github.com/cli/go-gh"
 	"github.com/cli/go-gh/pkg/api"
 	"log"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func getProjects(gqlclient api.GQLClient) []Project {
@@ -66,177 +60,50 @@ func main() {
 	}
 
 	projects := getProjects(gqlclient)
-
-	projectIds := make([]string, len(projects))
-	for i, node := range projects {
-		projectIds[i] = node.Id
-		if node.Id == "" {
-			log.Print(`[Warning] This extension requires permission for the "project" scope. You may not currently have permission to retrieve project information, please check`)
-		}
-	}
-
-	qs := []*survey.Question{
-		{
-			Name: "ProjectId",
-			Prompt: &survey.Select{
-				Message: "Choose a Project",
-				Options: projectIds,
-				Description: func(value string, index int) string {
-					return projects[index].Title + " (" + projects[index].Type + ")"
-				},
-				Filter: func(filterValue string, optValue string, optIndex int) bool {
-					return strings.Contains(projects[optIndex].Title, filterValue)
-				},
-			},
-		},
-	}
-	answers := struct{ ProjectId string }{}
-	err = survey.Ask(qs, &answers)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	projectId := answers.ProjectId
+	projectId := askOneProjectId(projects)
 	fields := getProjectFields(gqlclient, projectId)
 
 	itemTypes := []string{"Current PullRequest", "PullRequest", "Issue"}
-
-	var selectedType string
-	typeQuestion := &survey.Select{
-		Message: "Choose a Item Type",
-		Options: itemTypes,
-		Default: itemTypes[0],
-	}
-	survey.AskOne(typeQuestion, &selectedType)
+	selectedType := askOneContentType(itemTypes)
 
 	var itemId string
 	if selectedType == "Current PullRequest" {
 		currentPR := ghCurrentPullRequest()
 		itemId = addProject(gqlclient, projectId, currentPR.Id)
 	} else {
-		name := selectedType + " Number"
-		qs := []*survey.Question{
-			{
-				Name:   "number",
-				Prompt: &survey.Input{Message: name},
-				Validate: func(v interface{}) error {
-					strValue := v.(string)
-					_, err := strconv.Atoi(strValue)
-					if err != nil {
-						return errors.New("Value is Int")
-					}
-					return nil
-				},
-			},
-		}
-		answers := map[string]interface{}{}
-		err := survey.Ask(qs, &answers)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		content := ghContent(selectedType, answers["number"].(string))
+		number := askContentNumber(selectedType)
+		content := ghContent(selectedType, number)
 		itemId = addProject(gqlclient, projectId, content.Id)
 	}
+
 	for _, field := range fields {
 		if field.DataType == "TEXT" {
-			input := ""
-			prompt := &survey.Input{
-				Message: field.Name,
-			}
-			survey.AskOne(prompt, &input)
+			input := askTextFieldValue(field.Name)
 			if input != "" {
 				updateTextProjectField(gqlclient, projectId, itemId, field.Id, input)
 			}
 		}
 		if field.DataType == "DATE" {
-			qs := []*survey.Question{
-				{
-					Name:   field.Name,
-					Prompt: &survey.Input{Message: field.Name},
-					Validate: func(v interface{}) error {
-						strValue := v.(string)
-						// Allow Zero Value
-						if strValue == "" {
-							return nil
-						}
-						_, err := time.Parse("2006-01-02", strValue)
-						if err != nil {
-							return errors.New("Please format it like this '2006-01-02'")
-						}
-						return nil
-					},
-				},
-			}
-			answers := map[string]interface{}{}
-			err := survey.Ask(qs, &answers)
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
+			dateInput := askDateFieldValue(field.Name)
 
-			if answers[field.Name] != "" {
-				updateDateProjectField(gqlclient, projectId, itemId, field.Id, answers[field.Name].(string))
+			if dateInput != "" {
+				updateDateProjectField(gqlclient, projectId, itemId, field.Id, dateInput)
 			}
 		}
 		if field.DataType == "NUMBER" {
-			qs := []*survey.Question{
-				{
-					Name:   field.Name,
-					Prompt: &survey.Input{Message: field.Name},
-					Validate: func(v interface{}) error {
-						strValue := v.(string)
-						_, err := strconv.ParseFloat(strValue, 64)
-						if err != nil {
-							return errors.New("Value is Int or Float")
-						}
-						return nil
-					},
-				},
-			}
-			answers := map[string]interface{}{}
-			err := survey.Ask(qs, &answers)
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			f, _ := strconv.ParseFloat(answers[field.Name].(string), 64)
+			f := askNumberFieldValue(field.Name)
 			updateNumberProjectField(gqlclient, projectId, itemId, field.Id, f)
 		}
 		if field.DataType == "SINGLE_SELECT" || field.DataType == "ITERATION" {
-			fieldOptionSize := len(field.Options)
-			optionIds := make([]string, fieldOptionSize)
-			for i, opt := range field.Options {
-				optionIds[i] = opt.Id
-			}
-			qs := []*survey.Question{
-				{
-					Name: field.Name,
-					Prompt: &survey.Select{
-						Message: field.Name,
-						Options: optionIds,
-						Description: func(value string, index int) string {
-							return field.Options[index].Name
-						},
-						Filter: func(filterValue string, optValue string, optIndex int) bool {
-							return strings.Contains(field.Options[optIndex].Name, filterValue)
-						},
-					},
-				},
-			}
-			answers := map[string]string{}
-			err = survey.Ask(qs, &answers)
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-			if answers[field.Name] == "Skip" {
+			selected := askOneSelectFieldValue(field.Name, field.Options)
+
+			if selected == "Skip" {
 				continue
 			}
 			if field.DataType == "ITERATION" {
-				updateIterationProjectField(gqlclient, projectId, itemId, field.Id, answers[field.Name])
+				updateIterationProjectField(gqlclient, projectId, itemId, field.Id, selected)
 			} else {
-				updateSingleSelectProjectField(gqlclient, projectId, itemId, field.Id, answers[field.Name])
+				updateSingleSelectProjectField(gqlclient, projectId, itemId, field.Id, selected)
 			}
 		}
 	}
---
+++ survey.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"errors"
+	"github.com/AlecAivazis/survey/v2"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func askOneProjectId(projects []Project) (projectId string) {
+	projectIds := make([]string, len(projects))
+	for i, node := range projects {
+		projectIds[i] = node.Id
+		if node.Id == "" {
+			log.Print(`[Warning] This extension requires permission for the "project" scope. You may not currently have permission to retrieve project information, please check`)
+		}
+	}
+
+	qs := []*survey.Question{
+		{
+			Name: "ProjectId",
+			Prompt: &survey.Select{
+				Message: "Choose a Project",
+				Options: projectIds,
+				Description: func(value string, index int) string {
+					return projects[index].Title + " (" + projects[index].Type + ")"
+				},
+				Filter: func(filterValue string, optValue string, optIndex int) bool {
+					return strings.Contains(projects[optIndex].Title, filterValue)
+				},
+			},
+		},
+	}
+	answers := struct{ ProjectId string }{}
+	err := survey.Ask(qs, &answers)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return answers.ProjectId
+}
+
+func askOneContentType(itemTypes []string) string {
+	var selectedType string
+
+	typeQuestion := &survey.Select{
+		Message: "Choose a Item Type",
+		Options: itemTypes,
+		Default: itemTypes[0],
+	}
+	survey.AskOne(typeQuestion, &selectedType)
+
+	return selectedType
+}
+
+func askContentNumber(contentType string) string {
+	name := contentType + " Number"
+	qs := []*survey.Question{
+		{
+			Name:   "number",
+			Prompt: &survey.Input{Message: name},
+			Validate: func(v interface{}) error {
+				strValue := v.(string)
+				_, err := strconv.Atoi(strValue)
+				if err != nil {
+					return errors.New("Value is Int.")
+				}
+				return nil
+			},
+		},
+	}
+	answers := map[string]interface{}{}
+	err := survey.Ask(qs, &answers)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return answers["number"].(string)
+}
+
+func askTextFieldValue(fieldName string) string {
+	input := ""
+	prompt := &survey.Input{
+		Message: fieldName,
+	}
+	survey.AskOne(prompt, &input)
+
+	return input
+}
+
+func askDateFieldValue(fieldName string) string {
+	qs := []*survey.Question{
+		{
+			Name:   fieldName,
+			Prompt: &survey.Input{Message: fieldName},
+			Validate: func(v interface{}) error {
+				strValue := v.(string)
+				// Allow Zero Value
+				if strValue == "" {
+					return nil
+				}
+				_, err := time.Parse("2006-01-02", strValue)
+				if err != nil {
+					return errors.New("Please format it like this '2006-01-02'.")
+				}
+				return nil
+			},
+		},
+	}
+	answers := map[string]interface{}{}
+	err := survey.Ask(qs, &answers)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return answers[fieldName].(string)
+}
+
+func askNumberFieldValue(fieldName string) float64 {
+	qs := []*survey.Question{
+		{
+			Name:   fieldName,
+			Prompt: &survey.Input{Message: fieldName},
+			Validate: func(v interface{}) error {
+				strValue := v.(string)
+				_, err := strconv.ParseFloat(strValue, 64)
+				if err != nil {
+					return errors.New("Value is Int or Float.")
+				}
+				return nil
+			},
+		},
+	}
+
+	answers := map[string]interface{}{}
+	err := survey.Ask(qs, &answers)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	f, _ := strconv.ParseFloat(answers[fieldName].(string), 64)
+
+	return f
+}
+
+func askOneSelectFieldValue(fieldName string, options []Option) string {
+	fieldOptionSize := len(options)
+	optionIds := make([]string, fieldOptionSize)
+	for i, opt := range options {
+		optionIds[i] = opt.Id
+	}
+	qs := []*survey.Question{
+		{
+			Name: fieldName,
+			Prompt: &survey.Select{
+				Message: fieldName,
+				Options: optionIds,
+				Description: func(value string, index int) string {
+					return options[index].Name
+				},
+				Filter: func(filterValue string, optValue string, optIndex int) bool {
+					return strings.Contains(options[optIndex].Name, filterValue)
+				},
+			},
+		},
+	}
+	answers := map[string]string{}
+	err := survey.Ask(qs, &answers)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return answers[fieldName]
+}
